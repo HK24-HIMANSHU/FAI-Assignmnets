35ea508452ff3799823c397e553c65e62be8ea62
Himanshu kumar <56072567+HK24-HIMANSHU@users.noreply.github.com>
2023-04-22 22:38:40 +0530

    Add files via upload

---
+++ assignment01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimanshuAssignments
+{
+    class assignment01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Int Range is {int.MinValue} and {int.MaxValue}");
+            Console.WriteLine($"Double Range is {double.MinValue} and {double.MaxValue}");
+            Console.WriteLine($"Float Range is {float.MinValue} and {float.MaxValue}");
+            Console.WriteLine($"Short Range is {short.MinValue} and {short.MaxValue}");
+            Console.WriteLine($"Long Range is {long.MinValue} and {long.MaxValue}");
+            Console.WriteLine($" unsigned Short Range is {ushort.MinValue} and {ushort.MaxValue}");
+            Console.WriteLine($"Decimal Range is {decimal.MinValue} and {decimal.MaxValue}");
+        }
+    }
+}
