cf4e07e22d9400232ecc59536d1a1afd509fca03
Himanshu kumar <56072567+HK24-HIMANSHU@users.noreply.github.com>
2023-04-23 17:38:56 +0530

    Add files via upload

---
+++ assignment07.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimanshuAssignments
+{
+    class assignment07
+    {
+        static void Main(string[] args)
+        {
+            bool process = true;
+            do {
+                int num = UIconsole.GetInt("Enter number");
+                string result = isPrimeNumber(num) ? "Prime number" : "Not prime";
+                Console.WriteLine(result);
+                Console.WriteLine("Press any other key to clear screen");
+                Console.ReadKey();
+                Console.Clear();
+
+            } while (process);
+
+            
+
+        }
+        static Boolean isPrimeNumber(int num)
+        {
+            for(int i = 2; i < num / 2; i++)
+            {
+                if ((num % i) == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
