24ccdb7b70b7c5d0e6d95202669cfcc085912f33
Himanshu kumar <56072567+HK24-HIMANSHU@users.noreply.github.com>
2023-05-20 20:53:21 +0530

    Add files via upload

---
+++ SinglePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecapMVC.Controllers
+{
+    public class SinglePageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
